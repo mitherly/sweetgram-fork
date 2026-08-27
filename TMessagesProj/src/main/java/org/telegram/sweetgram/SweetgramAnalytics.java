a81ef032eb25211b2b1fd01cbaa254c65ff1a20f
mitherly <zaxarz751@gmail.com>
2026-08-27 21:11:46 +0300

    fix: import DataSnapshot in SweetgramAnalytics

--- TMessagesProj/src/main/java/org/telegram/sweetgram/SweetgramAnalytics.java
+++ TMessagesProj/src/main/java/org/telegram/sweetgram/SweetgramAnalytics.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
