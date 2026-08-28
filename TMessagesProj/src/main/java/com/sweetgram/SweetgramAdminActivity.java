bae2799009c612098cf2752594615d713e139e08
mitherly <zaxarz751@gmail.com>
2026-08-28 14:23:12 +0300

    fix: restore missing R import in SweetgramAdminActivity

--- TMessagesProj/src/main/java/com/sweetgram/SweetgramAdminActivity.java
+++ TMessagesProj/src/main/java/com/sweetgram/SweetgramAdminActivity.java
@@ -18,6 +18,7 @@ import androidx.annotation.NonNull;
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.FileLog;
 import org.telegram.messenger.LocaleController;
+import org.telegram.messenger.R;
 import org.telegram.ui.ActionBar.ActionBar;
 import org.telegram.ui.ActionBar.BaseFragment;
 import org.telegram.ui.ActionBar.Theme;
